af4639200dc383fdfd303287e07cc842b5ab8a2b
Ines Akrap <ines.akrap@netcentric.biz>
2023-08-17 09:38:48 +0200

    Initial code version

---
+++ index.js
@@ -0,0 +1,28 @@
+import { Router } from 'itty-router';
+import { co2 } from '@tgwf/co2';
+
+// Create a new router
+const router = Router();
+
+router.get("/", () => {
+  return new Response("Hello, world! This is the root page of your Worker template.")
+});
+
+router.get("/bytes/:value", ({params}) => {
+  const { value } = params
+
+  const emissions = new co2();
+  const result = emissions.perByte(value);
+
+  return new Response(JSON.stringify({result}), {
+    headers: {
+      "Content-Type": "application/json"
+    }
+  });
+});
+
+router.all("*", () => new Response("404, not found!", { status: 404 }));
+
+addEventListener('fetch', (e) => {
+  e.respondWith(router.handle(e.request))
+});
